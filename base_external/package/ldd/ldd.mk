5b7d35f12d5184925bbf4ac730e5e0e840926951
Geraldo Ribeiro <geraldolsribeiro@gmail.com>
2024-01-11 23:05:36 +0400

    Fix 7.2

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -1,20 +1,26 @@
-LDD_VERSION = '6bd1bc8bc05a2f1b74dc86f1d05b00cdedbe860d'
-LDD_SITE = 'git@github.com:geraldolsribeiro/ldd3.git'
+#LDD_VERSION = '6bd1bc8bc05a2f1b74dc86f1d05b00cdedbe860d'
+#LDD_SITE = 'git@github.com:geraldolsribeiro/ldd3.git'
+LDD_VERSION = '2b98cb61b1a6e9cd4b1b0ccebed5877f3d38aa2a'
+LDD_SITE = 'git@github.com:geraldolsribeiro/ecen-5713-assignment-7.2.git'
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
-LDD_MODULE_SUBDIRS = scull misc-modules
-LDD_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED) INSTALL_MOD_DIR=kernel/drivers/misc
+LDD_MODULE_SUBDIRS = misc-modules skull
+LDD_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED) #INSTALL_MOD_DIR=kernel/drivers/misc
+
+#define LDD_BUILD_CMDS
+	#$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/misc-modules
+	#$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/scull
+#endef
 
 define LDD_INSTALL_TARGET_CMDS
  	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
  	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
- 	$(INSTALL) -m 0755 $(@D)/scull/scull.init $(TARGET_DIR)/etc/init.d/S98lddmodules
  	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
  	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
- 	$(INSTALL) -m 0755 $(@D)/misc-modules/faulty.init $(TARGET_DIR)/etc/init.d/S99faultymodules
 endef
+
+LDD_POST_BUILD_HOOKS += LDD_INSTALL_TARGET_CMDS
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
-
